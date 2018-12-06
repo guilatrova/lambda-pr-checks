931437c8bea055ed1ac1ef71ba0484c43b844cb6
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-12-06 20:55:59 -0200

    Refactor test to keep it DRY

--- tests/unit/test_github.py
+++ tests/unit/test_github.py
@@ -1,28 +1,33 @@
 import os
 from unittest.mock import MagicMock
 
+import pytest
+
 from src import github
 
 
-def test_update_pr_status(mocker):
+@pytest.fixture()
+def expected_headers(mocker):
     mocker.patch.dict(os.environ, {"GITHUB_TOKEN": "123456"})
+    return github._get_gh_headers()
+
+
+def test_update_pr_status(expected_headers, mocker):
     request = mocker.patch.object(github.requests, "post", return_value=None)
 
-    headers = github._get_gh_headers()
     github.update_pr_status("url", "state", "context", "description")
 
     request.assert_called_once_with(
         "url",
         json={"context": "context", "state": "state", "description": "description"},
-        headers=headers,
+        headers=expected_headers,
     )
 
 
-def test_get_commits(mocker):
+def test_get_commits(expected_headers, mocker):
     mocker.patch.dict(os.environ, {"GITHUB_TOKEN": "123456"})
     request = mocker.patch.object(github.requests, "get", return_value=MagicMock())
 
-    headers = github._get_gh_headers()
     github.get_commits("url")
 
-    request.assert_called_once_with("url", headers=headers)
+    request.assert_called_once_with("url", headers=expected_headers)
