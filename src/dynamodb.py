1a75eeae561bf7b44fdcfde5560dc41030041f39
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-12-09 12:51:31 -0200

    Added log + table to template

--- src/dynamodb.py
+++ src/dynamodb.py
@@ -1,8 +1,16 @@
+import logging
+
 import boto3
 
 TABLE_NAME = "CodeFreezeConfig"
 FREEZE_CONFIG = "FreezeStatus"
 DEFAULT_STATUS = {"ConfigName": FREEZE_CONFIG, "Status": "disabled", "Author": ""}
+
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logging.basicConfig(level=logging.INFO)
 
 
 def _get_table():
@@ -12,6 +20,7 @@ def _get_table():
 
 
 def write_config(key, **kwargs):
+    logger.info("Writing key: " + key)
     table = _get_table()
     table.put_item(Item={"ConfigName": key, **kwargs})
 
