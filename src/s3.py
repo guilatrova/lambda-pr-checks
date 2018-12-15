d28ce2a65d71cdf861b3b0081d5523191a22ea01
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-12-15 16:44:34 -0200

    Added read file with no key handler

---
+++ src/s3.py
@@ -0,0 +1,26 @@
+import boto3
+from botocore.exceptions import ClientError
+
+BUCKET_NAME = "quality-reports"
+
+
+def _get_file(prefix, hash):
+    s3 = boto3.client("s3")
+
+    try:
+        response = s3.get_object(Bucket=BUCKET_NAME, Key=f"{prefix}/{hash}")
+    except ClientError as ex:
+        if ex.response["Error"]["Code"] == "NoSuchKey":
+            return False
+        else:
+            raise
+    else:
+        return response["Body"].read().decode("utf-8")
+
+
+def get_coverage_file(hash):
+    return _get_file("coverage", hash)
+
+
+def get_quality_file(hash):
+    return _get_file("quality", hash)
