56b9a148edf0627329330f8d2a44b330b793da4f
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-12-22 21:47:44 -0200

    Added dynamodb save_reports

--- src/dynamodb.py
+++ src/dynamodb.py
@@ -1,26 +1,52 @@
 import boto3
 
-TABLE_NAME = "CodeFreezeConfig"
+# CodeFreeze
+CODE_FREEZE_TABLE = "CodeFreezeConfig"
 FREEZE_CONFIG = "FreezeStatus"
-DEFAULT_STATUS = {"ConfigName": FREEZE_CONFIG, "Status": "disabled", "Author": ""}
+DEFAULT_CODEFREEZE_STATUS = {
+    "ConfigName": FREEZE_CONFIG,
+    "Status": "disabled",
+    "Author": "",
+}
+
+# Quality
+QUALITY_TABLE = "QualityReports"
 
 
-def _get_table():
+def _get_table(name):
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(TABLE_NAME)
+    table = dynamodb.Table(name)
     return table
 
 
+# CodeFreeze
 def write_config(key, **kwargs):
-    table = _get_table()
+    table = _get_table(CODE_FREEZE_TABLE)
     table.put_item(Item={"ConfigName": key, **kwargs})
 
 
 def get_code_freeze_config():
-    table = _get_table()
+    table = _get_table(CODE_FREEZE_TABLE)
     response = table.get_item(Key={"ConfigName": FREEZE_CONFIG})
 
     if "Item" in response:
         return response["Item"]
 
-    return DEFAULT_STATUS
+    return DEFAULT_CODEFREEZE_STATUS
+
+
+# Quality
+def save_reports(cov_report, quality_report, **kwargs):
+    table = _get_table(QUALITY_TABLE)
+
+    # Below line is just to be obvious and show the Key, it's not really required
+    commit_sha = kwargs.pop("commit_sha")
+
+    table.put_item(
+        Item={
+            "commit_sha": commit_sha,
+            "cov_report": cov_report,
+            "quality_report": quality_report,
+            **kwargs,
+        }
+    )
