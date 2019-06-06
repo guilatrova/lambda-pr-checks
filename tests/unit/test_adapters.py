ba4a6e7e6e05530b89180f72370a3969ade1b2e4
Guilherme Latrova <guilhermelatrova@hotmail.com>
2019-06-06 12:05:55 -0300

    Rename tests

--- tests/unit/test_adapters.py
+++ tests/unit/test_adapters.py
@@ -1,7 +1,7 @@
 from src import quality_summary
 
 
-def test_read_quality_file(mocker, flake8_qualitydiff_content):
+def test_flake8_read_quality_file(mocker, flake8_qualitydiff_content):
     mocker.patch.object(quality_summary.s3, "get_quality_file", return_value=flake8_qualitydiff_content)
 
     report = quality_summary._read_quality_file("")
@@ -31,7 +31,7 @@ def test_read_quality_file(mocker, flake8_qualitydiff_content):
     assert report["quality"] == "99%"
 
 
-def test_read_quality_file_single_line(mocker, flake8_qualitydiffsingle_content):
+def test_flake8_read_quality_file_single_line(mocker, flake8_qualitydiffsingle_content):
     mocker.patch.object(quality_summary.s3, "get_quality_file", return_value=flake8_qualitydiffsingle_content)
 
     report = quality_summary._read_quality_file("")
@@ -40,9 +40,39 @@ def test_read_quality_file_single_line(mocker, flake8_qualitydiffsingle_content)
     assert report["violations"] == "1"
 
 
-def test_read_quality_empty_file(mocker, flake8_qualitydiff_empty_content):
+def test_flake8_read_quality_empty_file(mocker, flake8_qualitydiff_empty_content):
     mocker.patch.object(quality_summary.s3, "get_quality_file", return_value=flake8_qualitydiff_empty_content)
 
     report = quality_summary._read_quality_file("")
 
     assert report is False
+
+
+def test_flake8_read_quality_file(mocker, flake8_qualitydiff_content):
+    mocker.patch.object(quality_summary.s3, "get_quality_file", return_value=flake8_qualitydiff_content)
+
+    report = quality_summary._read_quality_file("")
+
+    # Issues
+    assert len(report["issues"]) == 7
+    assert len(report["issues"][0]) == 4
+
+    # Issue in-depth
+    assert report["issues"][0]["file"] == "example/workers/feedback/models.py"
+    assert report["issues"][0]["line"] == "3"
+    assert report["issues"][0]["error_code"] == "F401"
+    assert report["issues"][0]["description"] == "'sqlalchemy.String' imported but unused"
+
+    # Files
+    assert len(report["files"]) == 13
+    assert len(report["files"][0]) == 2
+
+    # Files in-depth
+    assert report["files"][0]["file"] == "example/workers/feedback/models.py"
+    assert report["files"][0]["value"] == "80.0%"
+
+    # Overall
+    assert report["target_branch"] == "origin/dev"
+    assert report["total"] == "589"
+    assert report["violations"] == "5"
+    assert report["quality"] == "99%"
